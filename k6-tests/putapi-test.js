ebf18aedf9958c590374bf7f2bd9210e93d01e91
JCP12 <jc-poh@hotmail.com>
2025-08-20 08:33:37 +0100

    removed hardcoded payloads, now fetches from payloads JSON

--- k6-tests/putapi-test.js
+++ k6-tests/putapi-test.js
@@ -2,8 +2,10 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend } from 'k6/metrics';
 
-// Load the url from services file
+// Load the url and payload from services and payloads files
 const config = JSON.parse(open('./services/services.json'));
+const payload = JSON.parse(open('./payloads/requestTimetableChange.json'));
+
 const baseUrl = config.PutUrl;
 
 let responseTimeTrend = new Trend('response_time');
@@ -23,26 +25,8 @@ export let options = {
       },
 };
 
-
 export default function () {
   const url = `${baseUrl}/RequestTimetableChange`;
-
-  const payload = JSON.stringify({
-    timetableEventType: 0,
-    facultyName: "string",
-    moduleNumber: "string",
-    presentationPeriod: "string",
-    tutorialName: "string",
-    tutorialType: 0,
-    clusterOrTutorialGroupId: "string",
-    previousTutorialDate: "2025-08-14T09:01:50.167Z",
-    previousStartTime: "2025-08-14T09:01:50.167Z",
-    tutorialDate: "2025-08-14T09:01:50.167Z",
-    startTime: "2025-08-14T09:01:50.167Z",
-    endTime: "2025-08-14T09:01:50.167Z",
-    assignedTutors: ["string"],
-    maximumStudentAttendence: 0
-  });
 
   const params = {
     headers: {
@@ -50,7 +34,7 @@ export default function () {
     },
   };
 
-  const res = http.post(url, payload, params);
+  const res = http.put(url, JSON.stringify(payload), params);
 
   responseTimeTrend.add(res.timings.duration); // Record response time
 
